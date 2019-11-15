f3f45d20882f43c95f363729a6e5eb520e9bad04
John A Maloney <john.a.maloney@gmail.com>
2019-11-15 14:46:30 -0600

    Updated dir structure, made scraper module

--- server.js
+++ server.js
@@ -1,111 +1,58 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const request = require("request");
-const cheerio = require("cheerio");
-const fs = require("fs");
-const _ = require("underscore");
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const _ = require('underscore');
 const app = express();
 const port = process.env.PORT || 5000;
-const slack = require("./slack");
+const slack = require('./lib/slack');
+const scraper = require('./lib/scraper');
 
 const mtgoUrlBase = `https://magic.wizards.com/en/articles/archive/mtgo-standings/`;
 
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Yankee Hotel Foxtrot" });
+app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Yankee Hotel Foxtrot' });
 });
 
-app.get("/api/scrape", (req, res) => {
-  const { date, format, type = "league" } = req.query;
-  slack.send({
-    text: `
+app.get('/api/scrape', (req, res) => {
+    const { date, format, type = 'league' } = req.query;
+    slack.send({
+        text: `
     New Scraper Search!
     format: ${format},
     type: ${type},
     date: ${date}
-    `
-  });
-  try {
-    const jsonData = require(`./data/${format}-${type}-${date}.json`);
-    if (jsonData) {
-      res.send(jsonData);
-      return res.end();
-    }
-  } catch (e) {
-    console.log(`File not found. Going a'scraping now...`);
-  }
-  let url = `${mtgoUrlBase}/${format}-${type}-${date}#decklists`;
-  request(url, (err, re, html) => {
-    if (!err) {
-      const $ = cheerio.load(html);
-      let response = [];
-
-      const decklists = $(".deck-group");
-
-      const cardTypes = [
-        "creature",
-        "instant",
-        "sorcery",
-        "enchantment",
-        "planeswalker",
-        "artifact",
-        "land"
-      ];
-
-      const cardProps = ["qty", "name"];
-
-      const _extractCardData = $selector => {
-        return $selector
-          .text()
-          .split("\n\n")
-          .slice(2)
-          .map(card => {
-            const obj = {};
-            card
-              .split("\n")
-              .map(c => c.trim())
-              .filter(c => c !== "")
-              .map((c, i) => (obj[cardProps[i]] = c));
-            return obj;
-          })
-          .filter(c => Object.keys(c).length !== 0);
-      };
-
-      decklists &&
-        decklists.each((i, el) => {
-          const deck = { list: { sideboard: [] } };
-
-          deck.owner = $("h4", $(el))
-            .text()
-            .split(" (")[0];
-
-          cardTypes.forEach(type => {
-            const $selector = $(`.sorted-by-${type}`, $(el));
-            const typeData = _extractCardData($selector);
-            if (typeData.length) deck.list[type] = typeData;
-          });
-
-          const sideboard = $(".sorted-by-sideboard-container .row", $(el));
-          sideboard.each((i, sbEl) => {
-            const card = {};
-            card.qty = $(".card-count", $(sbEl)).text();
-            card.name = $(".card-name", $(sbEl)).text();
-            deck.list.sideboard.push(card);
-          });
-          response.push(deck);
-        });
-      if (response.length) fs.mkdirSync("data", { recursive: true });
-      fs.writeFile(
-        `./data/${format}-${type}-${date}.json`,
-        JSON.stringify(response, null, 4),
-        function(err) {
-          console.log("File successfully written!");
+    `,
+    });
+    try {
+        const jsonData = require(`./data/${format}-${type}-${date}.json`);
+        if (jsonData) {
+            res.send(jsonData);
+            return res.end();
         }
-      );
-      res.send(response);
-      res.end();
+    } catch (e) {
+        console.log(`File not found. Going a'scraping now...`);
     }
-  });
+    let url = `${mtgoUrlBase}/${format}-${type}-${date}#decklists`;
+    request(url, (err, re, html) => {
+        if (!err) {
+            const response = scraper(html);
+            if (response.length) {
+                fs.mkdirSync('data', { recursive: true });
+                fs.writeFile(
+                    `./data/${format}-${type}-${date}.json`,
+                    JSON.stringify(response, null, 4),
+                    function(err) {
+                        console.log('File successfully written!');
+                    }
+                );
+            }
+            res.send(response);
+            res.end();
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
