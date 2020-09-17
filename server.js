7a828ea6b6dea672bee7cf0034cefac6cb143e83
John A Maloney <john.a.maloney@gmail.com>
2020-09-17 16:22:47 -0500

    Change db config var name

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/mtgo-results',
-    { useNewUrlParser: true }
+  process.env.DB_URI || 'mongodb://localhost:27017/mtgo-results',
+  { useNewUrlParser: true }
 );
 
 const db = mongoose.connection;
@@ -29,51 +29,50 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const mtgoUrlBase = `https://magic.wizards.com/en/articles/archive/mtgo-standings/`;
 
 app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Yankee Hotel Foxtrot' });
+  res.send({ express: 'Yankee Hotel Foxtrot' });
 });
 
 app.get('/api/scrape', async (req, res) => {
-    const { date, format, type = 'league' } = req.query;
-    slack.send({
-        text: `
+  const { date, format, type = 'league' } = req.query;
+  slack.send({
+    text: `
     New Scraper Search!
     format: ${format},
     type: ${type},
     date: ${date}
     `,
-    });
-    try {
-        const hash = stringHash(`${format}-${type}-${date}`);
-        console.log(hash);
-        const dbResults = await Results.findOne({ hash });
-        if (dbResults) {
-            console.log(`Entry found! Sending...`);
-            res.send(dbResults.decklists);
-            return res.end();
-        }
-    } catch (e) {
-        console.log(e);
-        console.log(`Entry not found. Going a'scraping now...`);
+  });
+  try {
+    const hash = stringHash(`${format}-${type}-${date}`);
+    const dbResults = await Results.findOne({ hash });
+    if (dbResults) {
+      console.log(`Entry found! Sending...`);
+      res.send(dbResults.decklists);
+      return res.end();
     }
-    let url = `${mtgoUrlBase}/${format}-${type}-${date}#decklists`;
-    request(url, (err, re, html) => {
-        if (!err) {
-            const decklists = scraper(html);
-            if (decklists.length) {
-                const hash = stringHash(`${format}-${type}-${date}`);
-                new Results({
-                    format,
-                    type,
-                    date,
-                    decklists,
-                    hash,
-                }).save();
-            }
-            res.send(decklists);
-            console.log(`${decklists.length} decks scraped!`);
-            res.end();
-        }
-    });
+  } catch (e) {
+    console.log(e);
+    console.log(`Entry not found. Going a'scraping now...`);
+  }
+  let url = `${mtgoUrlBase}/${format}-${type}-${date}#decklists`;
+  request(url, (err, re, html) => {
+    if (!err) {
+      const decklists = scraper(html);
+      if (decklists.length) {
+        const hash = stringHash(`${format}-${type}-${date}`);
+        new Results({
+          format,
+          type,
+          date,
+          decklists,
+          hash,
+        }).save();
+      }
+      res.send(decklists);
+      console.log(`${decklists.length} decks scraped!`);
+      res.end();
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
